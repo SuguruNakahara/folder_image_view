b484f5f2a22f0e06181b7df08eeca37d6032f999
SuguruNakahara <subaru25.net@gmail.com>
2018-08-31 22:58:55 +0900

    image orientation fix

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-$php_file = 'index.php';
-
 function getFileList($dir) {
     $files = scandir($dir);
     $files = array_filter($files, function ($file) {
-        return !in_array($file, array('.', '..', $php_file));
+        return !in_array($file, array('.', '..', 'index.php'));
     });
 
     $list = array();
@@ -23,21 +21,39 @@ function getFileList($dir) {
 
 $current_dir = dirname(__FILE__);
 $list = getFileList($current_dir);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="utf-8"/>
+<style type="text/css">
+.rotate { transform: rotate(0deg) }
+.rotate_right { transform: rotate(90deg) }
+.rotate_left { transform: rotate(270deg) }
+</style>
 </head>
 <body>
-    <?php
-        $count = 0;
-        foreach ($list as $item) {
-            $path = str_replace(__DIR__, '', $item);
-            echo "<p>".$path."</p>";
-            echo "<img src='".$path."' width='210' height='210' />";
-        }
-    ?>
+<?php
+$count = 0;
+
+foreach ($list as $item) {
+    $path = str_replace(__DIR__, '.', $item);
+    $orientation = exif_read_data($path)["Orientation"];
+    if ($orientation == 6) {
+        $img_class = "rotate_right";
+    } else if ($orientation == 8) {
+        $img_class = "rotate_left";
+    } else {
+        $img_class = "rotate";
+    }
+    echo "<img class='".$img_class."' src='".$path."' width='210' height='210' />";
+
+    // Group with 3
+    $count++;
+    if ($count % 3 == 0) {
+        echo "<span style='margin-right:10px'></span>"; 
+    }
+}
+?>
 </body>
